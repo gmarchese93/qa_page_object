17467b5c578db0ffa4aa7a02dd4eb56d3c1b2e50
Gianluca Marchese <gianluca.marchese93@gmail.com>
2026-02-03 11:21:19 +0000

    qa2

--- src/pageObject.test.js
+++ src/pageObject.test.js
@@ -1,40 +1,31 @@
-class Header {
-  get logo() {
-    return 'Conduit logo';
-  }
-}
-
-class PageObject {
-  constructor(url) {
-    this.url = url;
-    this.header = new Header();
+src/pageObject.test.js
   }
 
   clickOnLogo() {
     return 'Click on the' + this.header.logo;
-  }
-}
+@mateacademy-ai-mentor
+mateacademy-ai-mentor
+now
+The concatenated string is missing a space between 'Click on the' and the logo text. This will result in 'Click on theConduit logo'. Consider adding a space to the string literal to make the sentence readable.
 
-class ArticlePage extends PageObject {
-  constructor(url, slug) {
-    super(url + slug);
-  }
-
-  get commentButton() {
-    return '[Publish comment] button';
+@gmarchese93	Reply...
+src/pageObject.test.js
   }
 
   clickOnCommentButton() {
     return 'Click on the' + this.commentButton;
+@mateacademy-ai-mentor
+mateacademy-ai-mentor
+now
+Similar to the clickOnLogo method, this string is missing a space. The output will be 'Click on the[Publish comment] button', which is not a properly formed sentence.
+
+@gmarchese93	Reply...
+src/pageObject.test.js
   }
 
   assertPageOpened() {
     return 'The ' + this.url + 'is opened';
-  }
-}
-
-module.exports = {
-  Header,
-  PageObject,
-  ArticlePage,
-};
+@mateacademy-ai-mentor
+mateacademy-ai-mentor
+now
+There's a missing space between the url property and the word 'is'. The current implementation will produce a string like 'The your/urlis opened'. A space should be added before 'is opened'.
